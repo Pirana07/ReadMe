4c2c8c4d0d62c92d3952b3831e9d0e45ddb9f1b4
Nika <144957020+Pirana07@users.noreply.github.com>
2023-09-14 20:50:31 +0400

    Create Cauculator.py

---
+++ Cauculator.py
@@ -0,0 +1,18 @@
+x = float(input("sheiyvanet ricxvi: "))
+z = input("sheiyvanet +,-,*,/ -->>  ")
+y = float(input("sheiyvanet meore ricxvi: "))
+
+if z == "+":
+	k = x + y
+	print(k)
+elif z == "-":
+	k = x - y
+	print(k)
+elif z == "*":
+	k = x * y
+	print(k)
+elif z == "/":
+	k = x / y
+	print(k)
+else:
+	print("Error")
