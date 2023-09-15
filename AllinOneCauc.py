283514c4196b706ed08ab45cfc356233fbf4141c
Nika <144957020+Pirana07@users.noreply.github.com>
2023-09-15 18:10:07 +0400

    Create AllinOneCauc.py

---
+++ AllinOneCauc.py
@@ -0,0 +1,11 @@
+def qauqulator(a, b):
+	jami = a + b
+	sxvaoba = a - b
+	namravli = a * b
+	gayofa = a / b 
+	return jami,sxvaoba,namravli,gayofa
+
+x = int(input("Please enter x: "))
+y = int(input("Please enter y: "))
+
+print(qauqulator(x, y))
