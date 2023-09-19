bb7fc13ad7368f6f8ff0426fc495ceed61763abd
Nika <144957020+Pirana07@users.noreply.github.com>
2023-09-20 00:00:16 +0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+fname = input("Enter first name: ") Iname = input("Enter last name: ")
+
+age = int(input("Enter age: "))
+
+adress = int(input("Enter adress: "))
+
+if age<=5:
+
+print("Child") elif age<=18:
+
+print("Young") elif age<=60:
+
+print("Adult") elif age<=120:
+
+print("Senior")
+
+print("Full name:" + fname + print("Age: " + age) + Inar
+
+print("Adress:" + adress)
